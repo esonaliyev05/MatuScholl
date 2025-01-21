714e963ec8a4ca58f7bf90502f9d190cb4b1c763
Alyorbekjonxon <alyorbek391@gmail.com>
2025-01-21 14:45:43 +0500

    plplp

--- src/Components/About_components/main/main.jsx
+++ src/Components/About_components/main/main.jsx
@@ -1,13 +1,17 @@
 import React from "react";
 import "./main.scss";
-import Swiper from "../../Home_components/Swiper/Swiper"
+import Swiper from "../../Home_components/Swiper/Swiper";
+import { GoCheck } from "react-icons/go";
 
 const Main = () => {
   return (
     <div className="Main-container">
       <div className="container">
         <div className="text-left">
-          <h3>Вы можете купить все рецепты со скидкой 30% <span>за 39 000 сум</span></h3>
+          <h3>
+            Вы можете купить все рецепты со скидкой 30%{" "}
+            <span>за 39 000 сум</span>
+          </h3>
 
           <button>приобрести и рецепты</button>
         </div>
@@ -18,30 +22,92 @@ const Main = () => {
 
         <div className="text-right">
           <h3>
-            Или вы можете купить все рецепты тортов со скидкой 30% <span>за 19 990</span>
+            Или вы можете купить все рецепты тортов со скидкой 30%{" "}
+            <span>за 19 990</span>
           </h3>
 
           <button>приобрести и рецепты</button>
         </div>
       </div>
 
-      
       <div className="Main-swiper">
-              
-              <div className="swiper-text">
-                   
-                   <h1>Ученики</h1>
+        <div className="swiper-text">
+          <h1>Ученики</h1>
 
-                   <h3>довольные результатом обучения</h3>
+          <h3>довольные результатом обучения</h3>
+        </div>
 
-              </div>
-              
-              <Swiper/>
-
+        <Swiper />
       </div>
 
+      <div className="main-reject">
+        <div className="container-reject">
+          <div className="text-top">
+            <h3>Что входит в</h3>
+            <h1>Рецепт</h1>
+          </div>
 
+          <div className="reject-parent">
+            <div className="reject-text">
+              <div className="box">
+                <div className="icon">
+                  {" "}
+                  <GoCheck />{" "}
+                </div>
 
+                <p>
+                  Купленные рецепты будут доступны в личном кабинете на сайте
+                </p>
+              </div>
+              <div className="box">
+                <div className="icon">
+                  {" "}
+                  <GoCheck />{" "}
+                </div>
+
+                <p>Технологические карты доступны к скачиванию</p>
+              </div>
+              <div className="box">
+                <div className="icon">
+                  {" "}
+                  <GoCheck />{" "}
+                </div>
+
+                <p>Доступ к урокам бесспрочный </p>
+              </div>
+            </div>
+
+            <div className="reject-img">
+              <img src="About-control/ohlov.png" alt="" className="img-old" />
+              <h3>
+                Подробный <br />
+                видеоурок
+              </h3>
+              <img src="About-control/prtret1.png" alt="" className="img-end" />
+              <img src="About-control/parta1.png" alt="" className="bg-img" />
+            </div>
+
+            <div className="reject-big-img">
+              <img src="About-control/blendir.png" alt="" className="old" />
+              <img src="About-control/qozon.png" alt="" className="end" />
+               
+               <h1>Технологическая <br />
+карта</h1>
+
+              <img
+                src="About-control/pichoq.png"
+                alt=""
+                className="top-img-mini"
+              />
+              <img src="About-control/parta2.png" alt="" className="bg-img" />
+            </div>
+                
+                <p className="text-reject">*точный состав урока вы найдете  <br />
+в описании каждого рецепта</p>
+
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
--- src/Page/Concat.jsx
+++ src/Page/Concat.jsx
@@ -1,24 +1,162 @@
-import React from "react";
-// import Concat from "../Components/ConcatTols/Concat";
-import "../Components/ConcatTols/Concat.scss"
+import React, { useEffect, useState } from "react";
+import "../Components/ConcatTols/Concat.scss";
 import { NavLink } from "react-router-dom";
 
 const Concat = () => {
+  const [formData, setFormData] = useState({
+    name: "",
+    phone: "",
+    message: "",
+  });
+
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [modalMessage, setModalMessage] = useState("");
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const TELEGRAM_BOT_TOKEN = "7866113672:AAGFq4SagMcM_x_AbjvFbb2nkzrOFoN8-Ho";
+    const TELEGRAM_CHAT_ID = "6992354984";
+
+    const telegramMessage = `
+📝 <b>Yangi habar:</b>
+
+👤 <b>Ism:</b> ${formData.name}
+📞 <b>Telefon:</b> ${formData.phone}
+💬 <b>Xabar:</b> ${formData.message}
+    `;
+
+    try {
+      const response = await fetch(
+        `https://api.telegram.org/bot${TELEGRAM_BOT_TOKEN}/sendMessage`,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            chat_id: TELEGRAM_CHAT_ID,
+            text: telegramMessage,
+            parse_mode: "HTML",
+          }),
+        }
+      );
+
+      if (response.ok) {
+        setModalMessage("✅ Xabar muvaffaqiyatli yuborildi!");
+        setFormData({ name: "", phone: "", message: "" });
+      } else {
+        throw new Error("Telegram API'da xatolik yuz berdi");
+      }
+    } catch (error) {
+      console.error(error);
+      setModalMessage("❌ Xabar yuborishda xatolik yuz berdi.");
+    } finally {
+      setIsModalOpen(true); // Modalni ochish
+    }
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false); // Modalni yopish
+  };
+
+
   return (
     <>
       <div className="Concat-main">
-
-       <div className="concat-logo">
-        
-        <NavLink to={"/"}>
+        <div className="concat-logo">
+          <NavLink to={"/"}>
             <img src="Nav-img/Mask group.png" alt="" />
-        </NavLink>
 
-       </div>
+            <h3>
+              MATU <br /> <span>school</span>
+            </h3>
+          </NavLink>
+        </div>
 
         <div className="container">
+          <div className="concat-text">
+            <h2>Напишите нам</h2>
 
+            <p>Режим работы тех.поддержки</p>
+
+            <h3>Пн-Сб с 9:00 до 19:00</h3>
+
+            <p>
+              Время ответа в течениие{" "}
+              <span>
+                24 <br /> часов
+              </span>{" "}
+              с момента обращения
+            </p>
+          </div>
+
+          <div className="form-container">
+            <form onSubmit={handleSubmit}>
+              <div className="input-group">
+                <label htmlFor="name">Ваше имя *</label>
+                <input
+                  type="text"
+                  id="name"
+                  name="name"
+                  value={formData.name}
+                  onChange={handleChange}
+                  placeholder="Введите ваше имя"
+                  required
+                />
+              </div>
+              <div className="input-group">
+                <label htmlFor="phone">Ваш телефон *</label>
+                <input
+                  type="tel"
+                  id="phone"
+                  name="phone"
+                  value={formData.phone}
+                  onChange={handleChange}
+                  placeholder="Введите ваш телефон"
+                  required
+                />
+              </div>
+              <div className="input-group">
+                <label htmlFor="message">Текст сообщения *</label>
+                <textarea
+                  id="message"
+                  name="message"
+                  value={formData.message}
+                  onChange={handleChange}
+                  placeholder="Оставьте свое сообщение"
+                  rows="4"
+                  required
+                ></textarea>
+              </div>
+              <button type="submit" className="submit-btn">
+                Отправить сообщение
+              </button>
+            </form>
+
+       
+          </div>
         </div>
+        {isModalOpen && (
+              <div className="modal">
+                <div className="modal-content">
+                  <p>{modalMessage}</p>
+                  <button onClick={closeModal} className="close-btn">
+                    Закрыть
+                  </button>
+                </div>
+              </div>
+            )}
+
+
       </div>
     </>
   );
